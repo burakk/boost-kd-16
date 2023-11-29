773353786190fe7dfd3c03eb642a6d139b45a8f0
Burak Kuyucaklı <burak@Buraks-Mac-Studio.local>
2023-11-29 12:31:12 +0300

    Add code to useReducer lesson

--- react/react-use-reducer/src/App.jsx
+++ react/react-use-reducer/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useState, useReducer } from 'react'
+import { tasksReducer } from './reducer/tasksReducer'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React useReducer</h1>
+      <TasksApp />
     </>
   )
 }
 
 export default App
+
+
+const DATA_TASKS = [
+  { id: 0, title: "Task 1" },
+  { id: 2, title: "Task 1" },
+  { id: 3, title: "Task 3" },
+]
+
+
+function TasksApp() {
+  const [tasks, dispatch] = useReducer(tasksReducer, DATA_TASKS);
+
+
+  return (
+    <ul>
+      {tasks.map(t => <li>{t.id} - {t.title}</li>)}
+      <button onClick={e => { dispatch({ type: "added", taskObj: { title: "Hello new task" } }) }}>ekle </button>
+    </ul>
+  )
+
+}
---
+++ react/react-use-reducer/src/reducer/tasksReducer.js
@@ -0,0 +1,22 @@
+export function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case "added": {
+      const newTasks = [...tasks, action.taskObj];
+      return newTasks;
+    }
+    case "deleted": {
+      const newTasks = tasks.filter((t) => t.id !== action.id);
+      return newTasks;
+    }
+    case "updated": {
+      const newTasks = tasks.map((t) => {
+        if (t.id === action.id) {
+          return { ...t, title: action.title };
+        }
+        return t;
+      });
+    }
+    default:
+      throw new Error("Action type not defined ");
+  }
+}
